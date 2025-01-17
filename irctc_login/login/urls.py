f414371e334414dad065dd0f29704d84c796688f
simran arora <140587380+simran1112-star@users.noreply.github.com>
2025-01-17 12:51:25 +0530

    Update urls.py

--- irctc_login/login/urls.py
+++ irctc_login/login/urls.py
@@ -1,3 +1,4 @@
+# when data through html forms
 from django.urls import path
 from . import views
 
@@ -6,3 +7,20 @@ urlpatterns = [
     path("login/", views.user_login, name="login"),
     path('home/', views.home, name='home'), 
 ]
+
+#when data through python forms
+#from django.urls import path
+#from . import views  # Importing views from the current application
+
+# URL patterns for the application
+#urlpatterns = [
+    # Route for the signup page
+    #path('signup/', views.signup_view, name='signup'),  
+    # Maps the URL `/signup/` to the `signup_view` function in `views.py`
+    # The `name='signup'` allows this route to be referred to by the name 'signup' in templates or reverse functions.
+
+    # Route for the login page
+    #path('login/', views.login_view, name='login'),  
+    # Maps the URL `/login/` to the `login_view` function in `views.py`
+    # The `name='login'` allows this route to be referred to by the name 'login' in templates or reverse functions.
+#]
